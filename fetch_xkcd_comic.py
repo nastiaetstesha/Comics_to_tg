4e41b8835e665f979dd4b90b135e74f17783e986
Avakova Anastasiia <avakova.ast@gmail.com>
2025-02-05 23:00:55 +0200

    initial

---
+++ fetch_xkcd_comic.py
@@ -0,0 +1,51 @@
+import os
+import requests
+
+from random import randint
+
+
+def get_xkcd_comic(comic_id="latest"):
+    base_url = "https://xkcd.com/"
+    url = f"{base_url}{comic_id}/info.0.json" if comic_id != "latest" else f"{base_url}info.0.json"
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    comic_data = response.json()
+    return {
+        "title": comic_data.get("title"),
+        "alt": comic_data.get("alt"),
+        "img_url": comic_data.get("img"),
+        "num": comic_data.get("num"),
+    }
+
+
+def get_random_xkcd_comic():
+    latest_comic = get_xkcd_comic(comic_id="latest")
+    max_comic_id = latest_comic['num']
+    random_comic_id = randint(0, max_comic_id)
+    return get_xkcd_comic(random_comic_id)
+
+
+def save_xkcd_comic(comic_data, save_directory):
+    os.makedirs(save_directory, exist_ok=True)
+    
+    img_url = comic_data["img_url"]
+    comic_id = comic_data["num"]
+    file_extension = os.path.splitext(img_url)[-1]
+    file_name = f"xkcd_{comic_id}{file_extension}"
+    save_path = os.path.join(save_directory, file_name)
+    
+    response = requests.get(img_url)
+    response.raise_for_status()
+    
+    with open(save_path, "wb") as file:
+        file.write(response.content)
+    
+    print(f"Комикс сохранён: {save_path}")
+    return save_path
+
+
+if __name__ == "__main__":
+    comic = get_xkcd_comic(353)
+    save_xkcd_comic(comic, "/Users/egorsemin/Practice/PublishComics/comics_img")
---
+++ tg_publisher.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from telegram.bot import Bot
+from fetch_xkcd_comic import save_xkcd_comic, get_random_xkcd_comic
+
+
+def publish_to_telegram_channel(token, channel_id, text=None, image_path=None):
+
+    bot = Bot(token=token)
+    if text:
+        bot.send_message(chat_id=channel_id, text=text)
+
+    if image_path:
+        with open(image_path, 'rb') as image:
+            bot.send_photo(chat_id=channel_id, photo=image)
+
+        os.remove(image_path)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    token = os.getenv("TG_ACCESS_TOKEN")
+    channel_id = os.getenv("TG_CHANNEL_ID")
+
+    if not token or not channel_id:
+        raise ValueError("TG_ACCESS_TOKEN или TG_CHANNEL_ID не указаны в .env файле")
+
+    comic = get_random_xkcd_comic()
+    image_path = save_xkcd_comic(comic, 'comics_img')
+
+    publish_to_telegram_channel(token, channel_id, text=comic['alt'], image_path=image_path)
+    print("Сообщение успешно отправлено в канал!")
